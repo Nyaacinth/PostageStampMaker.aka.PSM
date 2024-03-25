e6a15d23f091b3dc7719635cbccc9977f2ae69c9
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2024-03-25 12:33:06 +0800

    Add basic stamp construction

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,60 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Postage Stamp Maker',
+                style: TextStyle(
+                  fontFamily: "Sacramento",
+                  fontSize: 32,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.rotationZ(-0.03),
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(blurRadius: 10, color: Colors.grey.shade300),
+                    ],
+                  ),
+                  child: Container(
+                    color: Colors.grey.shade100,
+                    height: 200,
+                    width: 160,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ButtonBar(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Add Image'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Save Stamp'),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
